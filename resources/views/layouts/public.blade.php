21d0a40dce73e1f9f53966b9e3cc1adc421cac32
John Carlo Pasa <jcpasa922.pbox@parsu.edu.ph>
2026-09-21 22:30:24 +0800

    Test

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -12,6 +12,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
     <link href="{{ asset('css/celeste.css') }}" rel="stylesheet">
+    <link rel="icon" href="{{ asset('favicon.ico') }}?v=2" sizes="any">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('favicon-96x96.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}">
     @livewireStyles
 </head>
 <body>
